391845fd733ab6ccaa90760710aa6fa499e02383
nagendravarma <mudunuri1988@gmail.com>
2025-02-21 14:14:13 +0530

    removed unwanted client database tables

--- src/main/java/com/stepup/ims/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stepup.ims.repository;
-
-import com.stepup.ims.entity.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
