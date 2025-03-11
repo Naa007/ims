ea6c7ca5309f69ea0ff6de999f5451b86489ef15
NagendraMamidipalli <nagendra.mamidipalli@gmail.com>
2025-03-11 16:03:44 +0530

    Validations for employee and client management

--- src/main/java/com/stepup/ims/controller/EmployeeController.java
+++ src/main/java/com/stepup/ims/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import static com.stepup.ims.constants.UIRoutingConstants.REDIRECT_ADMIN_EMPLOYEE_MANAGEMENT;
 import static com.stepup.ims.constants.UIRoutingConstants.RETURN_TO_EMPLOYEE_MANAGEMENT;
@@ -31,7 +32,22 @@ public class EmployeeController {
 
     // Save Employee
     @PostMapping("/save-employee")
-    public String saveEmployee(@ModelAttribute Employee employee) {
+    public String saveEmployee(@ModelAttribute Employee employee, RedirectAttributes redirectAttributes) {
+        try {
+            if (employeeService.getEmployeeById(Long.valueOf(employee.getEmpId())).isPresent()) {
+                throw new IllegalArgumentException("Employee ID already exists");
+            }
+            employeeService.saveEmployee(employee);
+            return REDIRECT_ADMIN_EMPLOYEE_MANAGEMENT;
+        }catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return REDIRECT_ADMIN_EMPLOYEE_MANAGEMENT; // Adjust this to your redirect path
+        }
+    }
+
+    // Save Employee
+    @PostMapping("/update-employee")
+    public String updateEmployee(@ModelAttribute Employee employee) {
         employeeService.saveEmployee(employee);
         return REDIRECT_ADMIN_EMPLOYEE_MANAGEMENT;
     }
