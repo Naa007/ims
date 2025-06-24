13b93bfb15221bdaa4c87af5bfbfddfc15313e77
nagendravarma <mudunuri1988@gmail.com>
2025-06-24 19:03:19 +0530

    Inspection: Inspection Reports

--- src/main/java/com/stepup/ims/model/InspectionReports.java
+++ src/main/java/com/stepup/ims/model/InspectionReports.java
@@ -4,13 +4,17 @@ import com.stepup.ims.entity.Inspector;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 @Data
 public class InspectionReports {
     private Long id;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE, pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime reportDate;
+    @ToString.Exclude
     private Inspector inspector;
     private String reportNumber;
     private ReportType reportType;
